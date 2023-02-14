cfcb766828953362a44a888e7a16fe2454cf8ef3
Nascccom <ostrov_o@bk.ru>
2023-02-14 17:28:24 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./components/Header/Header";
+import {Main} from "./components/Main/Main";
+import {Skills} from "./components/Skills/Skills";
+import {MyWorks} from "./components/MyWorks/MyWorks";
+import {Contacts} from "./components/Contacts/Contacts";
+
+function App() {
+    return (
+      <div className="App">
+          <div className="Container">
+              <Header/>
+              <Main/>
+              <Skills/>
+              <MyWorks/>
+              <Contacts/>
+          </div>
+      </div>
+    );
+}
+
+export default App;

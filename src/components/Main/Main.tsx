30689eebb749fb05ea975d14e9745c47df15fd68
Nascccom <ostrov_o@bk.ru>
2023-06-27 23:54:25 +0300

    Created BackgroundEffect with style

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import style from './Main.module.css'
 import styleContainer from '../../common/styles/Container.module.css'
+import {BackgroundEffect} from "./Background_Effect/BackgroundEffect";
 
 export const Main = () => {
     return (
       <div className={style.mainBlock}>
+          <BackgroundEffect/>
+
           <div className={`${styleContainer.container} ${style.mainContainer}`}>
               <div className={style.photo}>
-                  <img src="photo.jpg" alt='my-photo'/>
+                  <img src="../../common/assets/photo.jpg" alt='my-photo'/>
               </div>
               <div className={style.title}>
                   <span>Hi, There</span>
